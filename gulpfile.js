c41f838c55f35915f40f7b5a820c5a6ea3b02b97
tyurin13 <batistabomb2010@gmail.com>
2020-07-24 15:11:59 +0300

    commit one

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    pug  = require('gulp-pug'),
+    browserSync = require('browser-sync').create()
+
+gulp.task('pug', function() {
+  return gulp.src('src/pug/**/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('build'))
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/sass/**/*.scss')
+  .pipe(sass({
+    // outputStyle: 'compressed'
+  }))
+  .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/pug/**/*.pug', gulp.series('pug'));
+  gulp.watch('src/sass/**/*.scss', gulp.series('scss'));
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./build"
+    }
+  });
+  browserSync.watch('build', browserSync.reload)
+});
+
+gulp.task('default', gulp.series(
+  gulp.parallel('pug', 'sass'),
+  gulp.parallel('watch', 'serve')
+));
